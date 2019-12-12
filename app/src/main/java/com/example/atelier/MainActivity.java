4870e86fe043703744504976025941ce0d559c18
Albert <albert.kokoshi@cit.edu.al>
2019-12-12 16:45:52 +0100

    first commit

---
+++ app/src/main/java/com/example/atelier/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.atelier;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //error0.1
+
+    }
+}
