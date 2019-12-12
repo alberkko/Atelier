21d10ad96cdc47b24a82825fe559605e850ac510
Albert <albert.kokoshi@cit.edu.al>
2019-12-12 18:01:09 +0100

    post_single_item added downgrade to PIE sdk 28
    todo -
    fix cardview

--- app/src/main/java/com/example/atelier/MainActivity.java
+++ app/src/main/java/com/example/atelier/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //error0.1
+        //error0.2
 
     }
 }
